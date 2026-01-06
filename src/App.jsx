90832d8c5a98478ea1c84ff74f839fe8308aa10d
PANDURANGZURE <pandurangzure6@gmail.com>
2026-01-06 17:45:50 +0530

    setect done

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,31 @@
 import React from 'react'
+import { useState } from "react";
 import { Button } from './Button'
 import { Input } from './Input'
 import { Navbar } from './Navbar'
 import { Textarea } from './Textarea'
 import Testing from './page/Testing'
+import { Select } from './Select'
 
 function App() {
-  let username = 'saurav'
+   const [country, setCountry] = useState("");
   return (
     <>
-    <Textarea
-  label="Feedback"
-  variant="outline"
-  color="purple"
-/>
-    <Testing/>
-
-<Textarea
-  label="Description"
-  variant='ghost'
-  size="lg"
-  resize="none"
-/>
-
-<Textarea
-  label="Error state"
-  error
+     <p>Selected: {country}</p>
+    <Select
+  label="Country"
+  value={country}
+  onChange={(e) => setCountry(e.target.value)}
+  options={[
+    { label: "India", value: "in" },
+    { label: "USA", value: "us" },
+  ]}
+  placeholder="Select country"
+  variant="outline"    // default | outline | ghost
+  size="md"            // sm | md | lg
+  color="#000000ff"
+  error={false}
+  disabled={false}
 />
 
     </>
