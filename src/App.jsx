5a41dd6496bb2690f871a49e3c56a3fd00d95620
PANDURANGZURE <pandurangzure6@gmail.com>
2026-01-06 22:49:26 +0530

    skeleton done

--- src/App.jsx
+++ src/App.jsx
@@ -11,15 +11,24 @@ import { Switch } from './Switch';
 import { Modal } from './Modal';
 import { Toast, toast } from "./Toast";
 import { Loader } from './Loaders';
+import { Skeleton, SkeletonCard, SkeletonText, SkeletonTable } from "./Skeleton";
 
 function App() {
   return (
     <>
-      <Loader/>
-      <Loader size="sm" />
-<Loader size="lg" color="green" />
-<Loader variant="dots" />
-<Loader variant="bar" color="#2563eb" />
+      <div style={{ padding: "20px", display: "flex", flexDirection: "column", gap: "20px" }}>
+      <h2>Basic Skeleton</h2>
+      <Skeleton width="200px" height="20px" />
+
+      <h2>Text Skeleton</h2>
+      <SkeletonText lines={4} />
+
+      <h2>Card Skeleton</h2>
+      <SkeletonCard />
+
+      <h2>Table Skeleton</h2>
+      <SkeletonTable rows={4} cols={3} />
+    </div>
     </>
   );
 }
