06f2d0bdf18243442149216a1ed09f6de341dbde
PANDURANGZURE <pandurangzure6@gmail.com>
2026-01-06 18:12:44 +0530

    switch

--- src/App.jsx
+++ src/App.jsx
@@ -7,25 +7,38 @@ import { Textarea } from './Textarea'
 import Testing from './page/Testing'
 import { Select } from './Select'
 import { Checkbox } from './Checkbox';
+import { Switch } from './Switch';
 
 function App() {
    const [country, setCountry] = useState("");
    const [checked, setChecked] = useState(false);
+   const [enabled, setEnabled] = useState(false);
   return (
     <>
-    <Checkbox
-  label="Accept Terms"
-  checked={checked}
-  onChange={(e) => setChecked(e.target.checked)}
-  variant="outline"
+    <Switch
+  label="Notifications"
+  checked={enabled}
+  onChange={(e) => setEnabled(e.target.checked)}
   size="md"
-  color="#2563eb"
-/>;
-    <Checkbox checked={checked} onChange={(e) => setChecked(e.target.checked)} label="Default" />
-<Checkbox label="Outline" variant="outline" />
-<Checkbox label="Solid" variant="solid" />
-
-
+  variant="solid"
+  color="#eb8f25ff"
+/>
+<Switch
+  label="Notifications"
+  checked={enabled}
+  onChange={(e) => setEnabled(e.target.checked)}
+  size="md"
+  variant="default"
+  color="#eb8f25ff"
+/>
+<Switch
+  label="Notifications"
+  checked={enabled}
+  onChange={(e) => setEnabled(e.target.checked)}
+  size="md"
+  variant="outline"
+  color="#141312ff"
+/>
     </>
   )
 }
