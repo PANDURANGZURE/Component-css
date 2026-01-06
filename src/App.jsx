bb4a2389de52ce95ba3545f53815f4d27d2d5e68
PANDURANGZURE <pandurangzure6@gmail.com>
2026-01-06 22:42:11 +0530

    loader

--- src/App.jsx
+++ src/App.jsx
@@ -10,11 +10,16 @@ import { Checkbox } from './Checkbox';
 import { Switch } from './Switch';
 import { Modal } from './Modal';
 import { Toast, toast } from "./Toast";
+import { Loader } from './Loaders';
 
 function App() {
   return (
     <>
-      
+      <Loader/>
+      <Loader size="sm" />
+<Loader size="lg" color="green" />
+<Loader variant="dots" />
+<Loader variant="bar" color="#2563eb" />
     </>
   );
 }
